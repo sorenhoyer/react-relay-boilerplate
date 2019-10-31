a04cc825162fcbed5918b7dfc6fbb1a357947210
sorenhoyer <sorenhoyer84@gmail.com>
2019-10-31 22:39:52 +0100

    Added missing shell scripts for installing required extensions for root and client

---
+++ scripts/install-or-update-required-vscode-extensions.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+code --install-extension editorconfig.editorconfig --force
+code --install-extension dbaeumer.vscode-eslint --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension prisma.vscode-graphql --force
+code --install-extension csstools.postcss --force
