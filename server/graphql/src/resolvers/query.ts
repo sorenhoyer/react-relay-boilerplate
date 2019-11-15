b0e30ca01e0032f4b17a1225b50a250873a5699e
sorenhoyer <sorenhoyer84@gmail.com>
2019-11-15 01:05:34 +0100

    Adding some fake delay to the graphql resolvers to test out suspense and fake slow network requests

--- server/graphql/src/resolvers/query.ts
+++ server/graphql/src/resolvers/query.ts
@@ -7,18 +7,22 @@ export default {
     me: async (_: unknown, __: unknown, context: Context): Promise<unknown> => {
       if (!context.me) return new AuthenticationError('Not authenticated!');
 
-      return context.me;
+      return new Promise(resolve => setTimeout(() => resolve(context.me), 4000));
     },
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     post: async (_: unknown, args: any, context: Context): Promise<unknown> => {
       if (!context.me) return new AuthenticationError('Not authenticated!');
 
-      return getPostById(args.id, context.me.id);
+      const post = await getPostById(args.id, context.me.id);
+      return new Promise(resolve => setTimeout(() => resolve(post), 2000));
+      // return getPostById(args.id, context.me.id);
     },
     posts: async (_: unknown, __: unknown, context: Context): Promise<unknown> => {
       if (!context.me) return new AuthenticationError('Not authenticated!');
 
-      return getPosts(context.me.id);
+      const posts = await getPosts(context.me.id);
+      return new Promise(resolve => setTimeout(() => resolve(posts), 8000));
+      // return getPosts(context.me.id);
     },
   },
   /** https://github.com/apollographql/apollo-server/issues/1075#issuecomment-427476421 */
